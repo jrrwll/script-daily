626a3cc00560c9d5ae4d9bd72259840db3c2ae44
tuke <jrriwll@gmail.com>
2025-06-27 16:45:53 +0800

    commit

--- shell/ops/docker.sh
+++ shell/ops/docker.sh
@@ -12,3 +12,16 @@ function docker_save_images() {
 function docker_find() {
     docker ps -a --format "{{.Names}}" | grep $1
 }
+
+function docker_untag() {
+     if [ -z $DOCKER_MIRROR ]; then
+         echo 'please define DOCKER_MIRROR first'
+         return 1
+     fi
+
+     for image in $(docker images | grep $DOCKER_MIRROR | awk '{print $1 ":" $2}'); do
+         real_image=$(echo $image | cut -d'/' -f2-)
+         echo docker tag $image $real_image
+         echo docker rmi $image
+     done
+}
